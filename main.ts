8e9b2f5299338ba6222856f7ed9b65432071b74f
2610229-wq <26_10229@shinmok.sen.ms.kr>
2026-06-19 11:05:06 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+input.onGesture(Gesture.Shake, function () {
+	
+})
+basic.showLeds(`
+    # # # . .
+    . . . # #
+    . . . # #
+    . . . # #
+    # # # . .
+    `)
+basic.forever(function () {
+	
+})
